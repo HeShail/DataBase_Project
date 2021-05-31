9fe720a92ef697630bd51cd885b905cc887ea053
Pepe <pepe.alpha97@gmail.com>
2021-05-31 23:47:37 +0200

    Desarrollo parcial de SQLite y Json.

---
+++ Assets/Script/Estudiante.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Estudiante : MonoBehaviour
+{
+    public string nombre;
+    public string don;
+    public int estatura_cm;
+    public string grupo_sanguineo;
+    public int notaAcceso;
+
+    public JObject Serialize()
+    {
+        string jsonString = JsonUtility.ToJson(this);
+        JObject retVal = JObject.Parse(jsonString);
+        return retVal;
+    }
+
+    public void Deserialize(string jsonString)
+    {
+        JsonUtility.FromJsonOverwrite(jsonString, this);
+    }
+}
